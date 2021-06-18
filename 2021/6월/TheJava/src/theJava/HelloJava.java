042b5badb8a50008effcb1a9058bd270ee47f619
EthanKim <hey0507@gmail.com>
2021-06-18 12:51:37 +0900

    about JVM

--- 2021/6월/TheJava/src/theJava/HelloJava.java
+++ 2021/6월/TheJava/src/theJava/HelloJava.java
@@ -1,3 +1,5 @@
+package theJava;
+
 public class HelloJava{
 	public static void main(String args[]){
 
---
+++ 2021/6월/TheJava/src/theJava/me/ethan/Ethan.java
@@ -0,0 +1,7 @@
+package theJava.me.ethan;
+
+public class Ethan {
+    public void work(){
+        System.out.println(App.myName);
+    }
+}
