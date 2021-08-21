426e0f9b95c80102272e62d41bf48228e5cc4f01
EthanKim <hey0507@gmail.com>
2021-08-21 11:08:19 +0900

    resatart in 21.aug

--- 2021/6월/TheJava/src/theJava/HelloJava.java
+++
@@ -1,9 +0,0 @@
-package theJava;
-
-public class HelloJava{
-	public static void main(String args[]){
-
-System.out.println("hello java");
-	}
-}
-
--- 2021/6월/TheJava/src/theJava/me/ethan/Ethan.java
+++
@@ -1,7 +0,0 @@
-package theJava.me.ethan;
-
-public class Ethan {
-    public void work(){
-        System.out.println(App.myName);
-    }
-}
